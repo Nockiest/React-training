9c44bc0728fad179b943c87ea5faac9568c9402d
Nockiest <115572770+Nockiest@users.noreply.github.com>
2023-03-09 19:07:48 +0100

    1 file modified, 1 added, 1 deleted

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+    return <nav>
+    <img src="../images/react-icon-small.png" />
+    <h3>ReactFacts</h3>
+    <h4>React Course - Project 1</h4>
+</nav>
 }
